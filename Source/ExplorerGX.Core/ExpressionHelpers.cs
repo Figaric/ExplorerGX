0c1f5b104924c02861df49e09ff264ebd45b65b0
figaric <sfigaric@gmail.com>
2021-05-01 14:26:52 +0300

    Add expression helpers
    A class that provides convenient usage of the Expressions

---
+++ Source/ExplorerGX.Core/ExpressionHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ExplorerGX.Core
+{
+    public static class ExpressionHelpers
+    {
+        public static T GetValue<T>(this Expression<Func<T>> lambda) => lambda.Compile().Invoke();
+
+        public static void SetValue<T>(this Expression<Func<T>> lambda, T value)
+        {
+            var expression = lambda.Body as MemberExpression;
+
+            var property = expression.Member as PropertyInfo;
+            var target = Expression.Lambda(expression.Expression).Compile().DynamicInvoke();
+
+            property.SetValue(target, value);
+        }
+    }
+}
