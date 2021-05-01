e63789a9e91dc8c00b7ef45ba4ff9d914fde5941
figaric <sfigaric@gmail.com>
2021-05-01 14:36:39 +0300

    Add mainwindow class

---
+++ Source/ExplorerGX.Desktop.Windows/Views/Pages/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ExplorerGX.Desktop.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
