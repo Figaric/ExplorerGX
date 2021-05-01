1cf3f414e27029e62c3631ec652a6d4d9e3768bc
figaric <sfigaric@gmail.com>
2021-05-01 14:23:59 +0300

    Add directorypanel.xaml
    A user control that represents a folder

---
+++ Source/ExplorerGX.Desktop.Windows/UserControls/DirectoryPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ExplorerGX.Desktop.Windows
+{
+    /// <summary>
+    /// Interaction logic for DirectoryPanel.xaml
+    /// </summary>
+    public partial class DirectoryPanel : UserControl
+    {
+        public DirectoryPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
