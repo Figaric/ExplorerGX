12feb5d24e059598be1e82e6403f8786a5019595
figaric <sfigaric@gmail.com>
2021-05-01 14:23:31 +0300

    Add sidetoolbar.xaml
    A user control that is docked to the left side of the main window, and there a user will be able to keep bookmarks

---
+++ Source/ExplorerGX.Desktop.Windows/UserControls/SideToolBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ExplorerGX.Desktop.Windows
+{
+    /// <summary>
+    /// Interaction logic for SideToolBar.xaml
+    /// </summary>
+    public partial class SideToolBar : UserControl
+    {
+        public SideToolBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
